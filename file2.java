6f9fd46f79f64513ba21b7418fe80f7f0a3d2ab9
sreedevi-del <sree03devi@gmail.com>
2025-03-07 13:49:05 +0530

    second commit

--- file2.java
+++ file2.java
@@ -2,7 +2,7 @@ package hello-git;
 
 public class file2 {
     public static void main(String[] args) {
-        System.out.println("welcome");
+        System.out.println("wlcome");
     }
     
 }
