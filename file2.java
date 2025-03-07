35e1777b100389d9c29fbecaa90c6e7029773c61
sreedevi-del <sree03devi@gmail.com>
2025-03-07 14:26:48 +0530

    Update file2.java

--- file2.java
+++ file2.java
@@ -2,8 +2,8 @@ package hello-git;
 
 public class file2 {
     public static void main(String[] args) {
-        System.out.println("wlcome");
-        System.out.println("hello");
+        System.out.println("welcome");
+        System.out.println("hello hello");
     }
     
 }
